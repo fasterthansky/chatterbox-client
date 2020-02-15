df6d305b2f452282d21cf132cc2c022c313f59d9
fasterthansky <fasterthansky@gmail.com>
2020-02-15 14:55:32 -0800

    created select option for roomlists

--- client/scripts/roomsView.js
+++ client/scripts/roomsView.js
@@ -14,10 +14,21 @@ var RoomsView = {
       this.roomlist[message.roomname] = message.roomname;
 
     }
+    for (var key in this.roomlist) {
+      this.html += RoomsView.renderRooms({roomname: this.roomlist[key]});
+    }
     //grab all rooms by roomname
     //add to selection box
     //selecting a roomname clears everything, repopulates with only messages of roomname property
+    this.$select.append(this.html);
 
-  }
+  },
+
+  renderRooms: _.template (
+    "<option class='option'>" +
+    "<%- roomname %>" +
+    "</option>"
+
+  )
 
 };
